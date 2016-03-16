f0f18b813dc1ca7e205a341ba239eea5370719bc
Yan <yans@yancomm.net>
2016-03-15 20:26:20 -0700

    tick version, dang you!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="deadbeef",
-    version="0.0.0.0.0.12.0.0.1.12.4.7.5",
+    version="0.0.0.0.0.12.0.1.14.12.4.7.5",
     author="Yan Shoshitaishvili",
     description="deadbeef makes it easy to generate hexspeak!",
     packages=['deadbeef']
